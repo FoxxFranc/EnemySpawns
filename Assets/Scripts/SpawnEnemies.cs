b1a4b3f953cd356a452e5d34a14f31900fc59c16
FoxxFranc <74159513+FoxxFranc@users.noreply.github.com>
2021-05-06 12:29:09 +0300

    Init

---
+++ Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    [SerializeField] private Enemy _template;
+    [SerializeField] private Transform _spawnPoints;
+
+    private Transform[] _points;
+    private float _spawnCooldown = 2f;
+
+    private void Start()
+    {
+        _points = new Transform[_spawnPoints.childCount];
+        for (int i = 0; i < _spawnPoints.childCount; i++)
+        {
+            _points[i] = _spawnPoints.GetChild(i);
+        }
+        StartCoroutine(SpawnUnits());
+    }
+
+    private IEnumerator SpawnUnits()
+    {
+        
+        for (int i = 0; i < _points.Length; i++)
+        {
+            Instantiate(_template, _points[i].position, Quaternion.identity);
+            yield return new WaitForSeconds(_spawnCooldown);
+        }
+    }
+}
+
